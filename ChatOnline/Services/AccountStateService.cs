6281999edf7552e9bb8d933f82860d4cc8b8f5d5
Камила Фаттахова <kamilafattahova65210@gmail.com>
2024-04-22 04:49:10 +0300

    Add project files.

---
+++ ChatOnline/Services/AccountStateService.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatOnline.Services
+{
+    public class AccountStateService
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Status { get; set; }
+        public string Avatar {  get; set; }
+    }
+}
