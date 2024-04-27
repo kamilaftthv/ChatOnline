2306b441955ae2518de5130bf0922eea7aea17b5
Камила Фаттахова <kamilafattahova65210@gmail.com>
2024-04-27 08:50:08 +0300

    beautyfull!

--- ChatOnline/Services/Chat.cs
+++ ChatOnline/Services/Chat.cs
@@ -6,18 +6,6 @@ namespace ChatOnline.Services
     {
         public User User { get; }
         public List<ChatMessage> Messages { get; } = new List<ChatMessage>();
-
-        public void UpdateMessages(List<ChatMessage> messages)
-        {
-            Messages.Clear();
-            Messages.AddRange(messages);
-        }
-
-        public void DeleteMessage(ChatMessage message)
-        {
-            Messages.Remove(message);
-        }
-
         public Chat(User user)
         {
             User = user;
